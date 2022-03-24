ede40d45fa96cd06498bf403f11f8466678c95da
simone <mondellosimo@gmail.com>
2022-03-24 15:28:20 +0100

    Esercizio.dadi

---
+++ index.html/main2.js
@@ -0,0 +1,23 @@
+
+	let ionum1=Math.round(Math.random()*6+1);
+    console.log(ionum1);
+	let computernum2=Math.round(Math.random()*6+1);
+	console.log(computernum2);		
+	
+
+	
+			
+	if(ionum1>computernum2){
+           alert("hai vinto");
+    }
+	else if(ionum1<computernum2) 
+    {
+	    alert("hai perso");
+    }
+
+    else (ionum1=computernum2);
+    {
+    alert('Parità');
+    }
+    
+console.log();
